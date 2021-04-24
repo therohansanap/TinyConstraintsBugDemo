017529d3a65ce1a5b6c637529592b145f760f860
Rohan Dilip Sanap <sanaprohan10@gmail.com>
2021-04-24 19:42:01 +0530

    Layout setup

--- TinyConstraintsBugDemo/ViewController.swift
+++ TinyConstraintsBugDemo/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import TinyConstraints
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    let signupButton = UIButton(type: .system)
+    signupButton.setTitle("Signup", for: .normal)
+    view.addSubview(signupButton)
+    signupButton.bottomToSuperview(offset: -50)
+    signupButton.trailingToSuperview(view.safeAreaLayoutGuide.centerXAnchor, offset: 20)
+
+    let loginButton = UIButton(type: .system)
+    loginButton.setTitle("Login", for: .normal)
+    view.addSubview(loginButton)
+    loginButton.bottomToSuperview(offset: -50)
+    loginButton.leadingToSuperview(view.safeAreaLayoutGuide.centerXAnchor, offset: 20)
   }
-
-
 }
-
